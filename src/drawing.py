99e49a9b236c059ebd9224d71f63379184635023
shadowcrafter <sharkpuncher08@gmail.com>
2026-03-23 12:29:25 -0400

    Improve visuals of selection a bit and add text instructions

--- src/drawing.py
+++ src/drawing.py
@@ -24,14 +24,17 @@ class AreaSelector:
         self.canvas.bind("<ButtonRelease-1>", self.on_button_release)
 
         self.root.bind("<Escape>", lambda _: self.root.destroy())
+        self.root.bind("<Return>", lambda _: self.root.destroy())
 
         self.canvas.focus_set()
+        self.canvas.create_text(self.root.winfo_screenwidth() / 2, self.root.winfo_screenheight() / 2, text="Click and drag to select area", font=("Arial", 16, "bold"))
     
     def on_button_press(self, event):
         self.canvas.delete("all")
+
         self.start_pos[0] = event.x
         self.start_pos[1] = event.y
-        self.rect = self.canvas.create_rectangle(self.start_pos[0], self.start_pos[1], 1, 1, outline="red", width=2)
+        self.rect = self.canvas.create_rectangle(self.start_pos[0], self.start_pos[1], 1, 1, outline="red", width=2, dash=(3, 20))
     
     def on_move(self, event):
         self.canvas.coords(self.rect, self.start_pos[0], self.start_pos[1], event.x, event.y) #gets the rectangle and updates it
@@ -43,6 +46,7 @@ class AreaSelector:
         self.selection = (x1, y1, x2 - x1, y2 - y1)
 
         self.canvas.itemconfigure(self.rect, fill="green", stipple="gray50", outline="green")
+        self.canvas.create_text(x1, y1, text="Press ESCAPE or RETURN to confirm selection.", font=("Arial", 12, "bold"), anchor="sw")
     
     def get_selection(self):
         self.root.mainloop()
